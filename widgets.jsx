00a5e7e4ef1825d934896925041df8132f50f905
xmagnetisx <muhammad.trad@yahoo.com>
2020-04-03 21:14:11 -0700

    add tabs component

--- widgets.jsx
+++ widgets.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './frontend/clock';
+import Tabs from  './frontend/tabs';
 
+const panes = [
+  {title: 'one', content: 'This is the first tab!'},
+  {title: 'two', content: 'This is the second tab!'},
+  {title: 'three', content: 'This is the third tab!'}
+];
+
+function Root(){
+  return(
+    <div>
+      <Clock />
+      <div className='interactive'>
+      <Tabs panes={panes}/>
+
+      </div>
+    </div>
+  );
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<Clock/>, root);
+  ReactDOM.render(<Root/>, root);
 });
