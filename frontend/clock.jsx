62868ac1a47edf546509e8479bc565a187dbb7c1
xmagnetisx <muhammad.trad@yahoo.com>
2020-04-03 18:12:28 -0700

    add clock component

--- frontend/clock.jsx
+++ frontend/clock.jsx
@@ -1,34 +1,50 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 
 class Clock extends React.Component {
     constructor(props){
         super(props);
         this.state = { time: new Date() };
-        this.id = id;
-
         this.tick = this.tick.bind(this);
-        this.componentDidMount = this.componentDidMount.bind(this);
-        this.componentWillUnmount = this.componentWillUnmount.bind(this);
     }
-
-    tick(){
-        let time = new Date();
-        this.setState({ time });
-    }
-
     componentDidMount(){
-
+        this.Id = setInterval(this.tick, 1000);
     }
-
     componentWillUnmount(){
-
+        clearInterval(this.Id);
+    }
+    tick(){
+        this.setState({ time : new Date()});
     }
 
     render(){
+        const { time } = this.state;
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds();
+
+        hours = (hours < 10) ? `0${hours}` : hours;
+        minutes = (minutes < 10) ? `0${minutes}` : minutes;
+        seconds = (seconds < 10) ? `0${seconds}` : seconds;
+
         return (
             <div>
                <h1>Clock</h1> 
+               <div className ='clock'>
+                   <p><span>
+                        Time:
+                   </span>
+                   <span>
+                       {hours}:{minutes}:{seconds} PDT
+                   </span>
+                   </p>
+                  <p><span>
+                      Date:
+                  </span>
+                  <span>
+                      {time.toDateString()}
+                  </span>
+                  </p>
+               </div>
             </div>
         );
     }
